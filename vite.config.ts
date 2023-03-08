0bb988cd0a9075366a7672dec3721649059b1a99
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-03-08 20:47:11 +0800

    0.3 fixed bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import msw from '@iodigital/vite-plugin-msw'
 
 // https://vitejs.dev/config/
 export default defineConfig({
