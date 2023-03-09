b62f64e5387ce97dee0c0beb8eddef9fe0331e36
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-03-09 23:32:39 +0800

    1.0

---
+++ plugins/service.ts
@@ -0,0 +1,49 @@
+import type { Plugin } from "vite";
+import * as fs from "fs";
+
+
+function writeServiceWorker(caches: string[]): void {
+    const data = `
+const version = "1.0";
+
+self.addEventListener("install", (evt) => {
+  self.skipWaiting().then(r => undefined);
+  evt.waitUntil(caches.open(version).then((cache) => {
+    return cache.addAll(${JSON.stringify(caches)});
+  }));
+});
+
+self.addEventListener("fetch", (evt) => {
+  const url = new URL(evt.request.url);
+  evt.respondWith(caches.match(
+    (url.host === "fystart.deeptrain.net" && url.pathname === "/") ? "index" : evt.request
+  ).then((response) => {
+    return response || fetch(evt.request).catch(() => 0);
+  }).then((data) => {
+    return data || new Response(null, { status: 502, statusText: "Bad Gateway" });
+  }));
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(caches.keys().then((cacheNames) => {
+    return Promise.all(cacheNames.map((cacheName) => {
+      if (cacheName !== version) return caches.delete(cacheName);
+    }));
+  }));
+});`
+    fs.writeFile('./dist/service.js', data, 'utf8', () => 0);
+
+}
+export default function serviceWorkerPlugin(): Plugin {
+    return {
+        name: 'vite:service-worker',
+        closeBundle () {
+            fs.readFile('./dist/manifest.json', 'utf8', (err, data) => {
+                if (err) throw err; /** @ts-ignore **/
+                const caches = Object.values(JSON.parse(data)).map((data: {file: string}) => data['file']);
+                caches.push("background.jpg", "favicon.ico", "index");
+                writeServiceWorker(caches);
+            })
+        }
+    }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import service from './plugins/service'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    service(),
   ],
+  build: {
+    manifest: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
