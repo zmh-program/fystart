e21bf97957f92b350173a1829faec9eb5c6903e8
zmh-program <zmh13054618081@dingtalk.com>
2023-05-30 15:25:35 +0800

    update time widget

---
+++ src/components/TimeWidget.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { uptime } from "@/assets/script/utils";
+
+const time = uptime();
+</script>
+
+<template>
+  <div class="time">
+    <span>{{ time }}</span>
+  </div>
+</template>
+
+<style scoped>
+.time {
+  position: absolute;
+  top: 80px;
+  transition: .25s;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.time span {
+  color: #fff;
+  font-family: "Nunito", monospace;
+  font-weight: 400;
+  font-size: 42px;
+  text-shadow: 0 0 20px rgba(0, 0, 0, .35);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  transition: .25s;
+  user-select: none;
+}
+
+.time:hover,
+.time:focus {
+  cursor: pointer;
+  transform: translateY(-4px) translateX(-50%);
+}
+</style>
