923cbde9a81a0999afb72e6c2326abcb00fce7b4
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-25 15:08:46 +0800

    add html minify

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import service from './plugins/service'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     service(),
+    createHtmlPlugin({
+      minify: true,
+    }),
   ],
   build: {
     manifest: true,
